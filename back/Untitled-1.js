20e978fbc4850b48f7a77c927f2799e736682d0c
vergilssssv <93860378+vergilssssv@users.noreply.github.com>
2021-12-13 17:16:52 +0800

    Add files via upload

--- back/Untitled-1.js
+++ back/Untitled-1.js
@@ -1,51 +1,8 @@
-var i=0;
-function setBGC() {
-	i++;
-	switch(i){
-			case 1:document.getElementById("imgBg").style.background = "#ff0000";
-			break;
-			case 2:document.getElementById("imgBg").style.background = "#800000";
-			break;
-			case 3:document.getElementById("imgBg").style.background = "#00ff00";
-			break;
-			case 4:document.getElementById("imgBg").style.background = "#008000";
-			break;
-			case 5:document.getElementById("imgBg").style.background = "#0000ff";
-			break;
-			case 6:document.getElementById("imgBg").style.background = "#000080";
-			break;
-			case 7:document.getElementById("imgBg").style.background = "#ff00ff";
-			break;
-			case 8:document.getElementById("imgBg").style.background = "#ffff00";
-			break;
-			case 9:document.getElementById("imgBg").style.background = "#00ffff";
-			break;
-			case 10:document.getElementById("imgBg").style.background = "#808080";
-			break;
-			case 11:document.getElementById("imgBg").style.background = "#800080";
-			break;
-			case 12:document.getElementById("imgBg").style.background = "#808000";
-			break;
-			case 13:document.getElementById("imgBg").style.background = "#008080";
-			break;
-			case 14:document.getElementById("imgBg").style.background = "#ffc0c0";
-			break;
-			case 15:document.getElementById("imgBg").style.background = "#c0ffc0";
-			break;
-			case 16:document.getElementById("imgBg").style.background = "#c0c0ff";
-			break;
-			case 17:document.getElementById("imgBg").style.background = "#c0ffff";
-			break;
-			case 18:document.getElementById("imgBg").style.background = "#ffc0ff";
-			break;
-			case 19:document.getElementById("imgBg").style.background = "#ffffc0";
-			break;
-			default:i=0;
-
+function setBGC(){
+	var x=prompt("请输入6位颜色代码")
+	var BGC
+	if(x!=null && x!=""){
+		BGC="#"+x 
+		document.getElementById("imgBg").style.background = BGC 
 	}
-	
-	}
-
-
-
-//setInterval(setBGC, 300);
+}
